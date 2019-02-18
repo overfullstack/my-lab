34536e747f10caaf180c427f03a7441821fb2416
Gopal S Akshintala <gakshintala@salesforce.com>
2019-02-18 14:48:31 +0530

    commit

--- src/test/java/FunctionLab.java
+++ src/test/java/FunctionLab.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.function.Function;
 
-public class FunctionLab {
+class FunctionLab {
     
     @Test
     void functionChain() {
--- src/test/java/StreamLab.java
+++ src/test/java/StreamLab.java
@@ -1,34 +1,37 @@
 /*
- * Copyright (c) 2018 - Present, Gopal S Akshintala 
+ * Copyright (c) 2019 - Present, Gopal S Akshintala 
  * This source code is licensed under the Creative Commons Attribution-ShareAlike 4.0 International License.
  * 	http://creativecommons.org/licenses/by-sa/4.0/
  */
 
-package lambda;
-
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Optional;
 import java.util.stream.IntStream;
+import org.junit.jupiter.api.Test;
 
-public class StreamLab {
-    public static void main(String[] args) {
-        //generate();
-        findFirstTest();
-    }
+class StreamLab {
 
-    private static void generate() {
+    @Test
+    void generate() {
         Collections.nCopies(8, 1).stream().forEach(System.out::print);
         System.out.println();
         IntStream.generate(() -> 1).limit(8).forEach(System.out::print);
     }
 
-    private static void findFirstTest() {
+    @Test
+    void findFirstTest() {
         Integer[] arr = {1,2,3,6};
         Arrays.stream(arr)
                 .filter(i -> i > 5)
                 .map(Optional::of)
         ;
+    }
+    
+    @Test
+    void findFirstForEmpty() {
+        System.out.println(new ArrayList<>().stream().findFirst());
     }
     
 }
