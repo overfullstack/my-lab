a2cb1c328e552b935cdd694248816f896253faa1
Gopal S Akshintala <gakshintala@salesforce.com>
2019-07-04 17:31:43 +0530

    commit

--- src/test/java/labs/StreamLab.java
+++ src/test/java/labs/StreamLab.java
@@ -13,8 +13,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
--- src/test/java/vavr/CollectionsLab.java
+++ src/test/java/vavr/CollectionsLab.java
@@ -2,6 +2,7 @@ package vavr;
 
 import common.Bean;
 import io.vavr.collection.List;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.Collections;
@@ -9,9 +10,45 @@ import java.util.function.Function;
 
 public class CollectionsLab {
 
+    private static List<Bean> beans;
+
+    @BeforeAll
+    static void setUp() {
+        beans = List.of(
+                new Bean("1", "a", Collections.emptyList()),
+                new Bean("2", "b", Collections.emptyList()),
+                new Bean("3", "c", Collections.emptyList())
+        );
+    }
+
     @Test
-    void reduceApply() {
-        List<Function<Bean, String>> functionList = List.of(Bean::getProp1, Bean::getProp2, bean -> bean.getProp1() + bean.getProp2());
+    void foldApply() {
+        List<Function<Bean, Bean>> functionList = List.of(
+                bean -> {bean.setProp1(bean.getProp1() + ":11"); return bean;},
+                bean -> {bean.setProp1(bean.getProp1() + ":22"); return bean;},
+                bean -> {bean.setProp1(bean.getProp1() + ":33"); return bean;}
+        );
         var bean = new Bean("1", "A", Collections.emptyList());
+        final var resultBean = functionList.foldLeft(bean, (acc, cur) -> cur.apply(acc));
+        System.out.println(resultBean);
+    }
+
+    @Test
+    void listFoldLeft() {
+        final var result = beans.foldLeft("start", (acc, cur) -> {
+            System.out.println("acc: " + acc);
+            System.out.println("cur: " + cur);
+            return acc + " - " + cur;
+        });
+        System.out.println(result);
+    }
+
+    @Test
+    void listReduce() {
+        beans.reduce((prev, cur) -> {
+            System.out.println("prev: " + prev);
+            System.out.println("cur: " + cur);
+            return cur;
+        });
     }
 }
