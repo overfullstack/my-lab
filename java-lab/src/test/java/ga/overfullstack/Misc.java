6f7cc66d77f62dd71f6be9bd21085c41927e6473
Gopal S Akshintala <gopala.akshintala@salesforce.com>
2021-11-29 15:21:13 +0530

    commit

--- java-lab/src/test/java/ga/overfullstack/Misc.java
+++ java-lab/src/test/java/ga/overfullstack/Misc.java
@@ -1,12 +1,13 @@
 package ga.overfullstack;
 
 import org.junit.jupiter.api.Test;
+import java.util.Objects;
 
 class Misc {
   @Test
   void exceptionInCatch() {
     try {
-      throw new RuntimeException();
+      someThrowingFun();
     } catch (Exception e) {
       throw new RuntimeException();
     } finally {
@@ -14,4 +15,18 @@ class Misc {
     }
   }
   
+  void someThrowingFun() {
+    try {
+      throw new RuntimeException();
+    } finally {
+      System.out.println("finally - someFun");
+    }
+  }
+
+  @Test
+  void testIntCast() {
+    Object obj = null;
+    final var i = (int) Objects.requireNonNullElse(obj, 0);
+    System.out.println(i);
+  }
 }
