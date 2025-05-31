b3b48f68d7864041209cffcc93696c1b2b1ab08f
Gopal S Akshintala <gopala.akshintala@salesforce.com>
2025-05-31 16:52:55 +0530

    commit
    Signed-off-by: Gopal S Akshintala <gopala.akshintala@salesforce.com>

---
+++ java-lab/src/main/java/ga/overfullstack/v65/ClassWithParametricTypes.java
@@ -0,0 +1,7 @@
+package ga.overfullstack.v65;
+
+public abstract class ClassWithParametricTypes<T, U> {
+  protected final java.lang.reflect.Type type1 =
+      new com.google.common.reflect.TypeToken<T>(getClass()) {}.getType(); // or getRawType() to return Class<? super T>
+  protected final java.lang.reflect.Type type2 = new com.google.common.reflect.TypeToken<U>(getClass()) {}.getType();
+}
